024cf7a1fcf28481f90764d3f2f8df6bc17a8ae2
georg <georgkoltwig@gmail.com>
2023-07-07 15:33:36 +0200

    v0.1.23

---
+++ src/main/java/de/laurinhummel/skyguns/listeners/TestPlayerShootParticleListener.java
@@ -0,0 +1,43 @@
+package de.laurinhummel.skyguns.listeners;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+public class TestPlayerShootParticleListener implements Listener {
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if(!event.getAction().equals(Action.LEFT_CLICK_AIR)) { return; }
+        if(!Objects.equals(event.getItem(), new ItemStack(Material.DIAMOND_HOE))) { return; }
+        Player player = event.getPlayer();
+
+        int i = 0;
+
+        Location loc = player.getEyeLocation();
+        double maxLength = 20;
+        for(double d = 2; d <= maxLength; d++) {
+            loc.add(loc.getDirection());
+            if(!loc.getBlock().isPassable()) { return; }
+            player.spawnParticle(Particle.VILLAGER_HAPPY, loc, 1, 0, 0, 0);
+
+                for(Entity entity : loc.getWorld().getNearbyEntities(loc, 0.5, 1, 0.5)) {
+                    if(entity instanceof Monster) {
+                        ((LivingEntity) entity).damage(200);
+                        player.sendMessage("!");
+                        i++;
+                        if(i == 2) { return; }
+                    }
+                }
+        }
+    }
+}
---
+++ src/main/java/de/laurinhummel/skyguns/main/Main.java
@@ -0,0 +1,31 @@
+package de.laurinhummel.skyguns.main;
+
+import de.laurinhummel.skyguns.listeners.TestPlayerShootParticleListener;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    private static Main plugin;
+
+    @Override
+    public void onLoad() {
+        plugin = this;
+    }
+    @Override
+    public void onEnable() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+
+        pluginManager.registerEvents((Listener) new TestPlayerShootParticleListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public static Main getPlugin() {
+        return plugin;
+    }
+}
