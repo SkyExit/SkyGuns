a0a8dd60ce860a863da3add7825a1e05da5b78b4
georg <georgkoltwig@gmail.com>
2023-08-11 15:33:22 +0200

    v0.5.0

--- src/main/java/de/laurinhummel/skyguns/listeners/WeaponShootListenerV2.java
+++ src/main/java/de/laurinhummel/skyguns/listeners/WeaponShootListenerV2.java
@@ -24,18 +24,18 @@ public class WeaponShootListenerV2 implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         if(!event.getAction().equals(Action.LEFT_CLICK_AIR)) { return; }
+
         WeaponManager weaponManager = new WeaponManager(event.getItem());
         if(!weaponManager.isWeapon()) { return; }
-        Player player = event.getPlayer();
         Weapon weapon = weaponManager.getWeapon();
+
+        Player player = event.getPlayer();
 
         if (Cooldown.checkCooldown(event.getPlayer())) {
             Cooldown.setCooldown(event.getPlayer(), weapon.getDelay()); //Sets a cooldown
         } else { return; }
 
-        player.setCooldown(Material.GOLDEN_HOE, weapon.getDelay()/50);
-
-        int i = 0;
+        player.setCooldown(Material.GOLDEN_HOE, weapon.getDelay() / 50);  //convert ms into minecraft ticks   (1s = 20ticks   -->   1000ms / 50 = 20ticks)
 
         Location loc2 = player.getEyeLocation();
         player.playSound(loc2, Sound.UI_BUTTON_CLICK, 1, 1);
@@ -43,26 +43,24 @@ public class WeaponShootListenerV2 implements Listener {
         double maxLength = 20;
         for(double d = 2; d <= maxLength; d++) {
             loc2.add(loc2.getDirection());
-            //if(!loc2.getBlock().isPassable()) { return; }
+            if(!loc2.getBlock().isPassable()) { continue; }
             player.spawnParticle(Particle.VILLAGER_HAPPY, loc2, 1, 0, 0, 0);
         }
 
-        List<Entity> nearbyE = player.getNearbyEntities(20,
-                20, 20);
-        ArrayList<LivingEntity> livingE = new ArrayList<LivingEntity>();
+        List<Entity> nearbyE = player.getNearbyEntities(20, 20, 20);
+        ArrayList<LivingEntity> livingE = new ArrayList<>();
 
         for (Entity e : nearbyE) {
-            if (e instanceof LivingEntity) {
+            if (e instanceof LivingEntity && ((LivingEntity) e).getHealth() > 0) {
                 livingE.add((LivingEntity) e);
             }
         }
 
-        LivingEntity target = null;
         BlockIterator bItr = new BlockIterator(player, 20);
         Block block;
         Location loc;
-        int bx, by, bz = 0;
-        double ex, ey, ez = 0;
+        int bx, by, bz;
+        double ex, ey, ez;
         // loop through player's line of sight
         while (bItr.hasNext()) {
             block = bItr.next();
@@ -77,11 +75,10 @@ public class WeaponShootListenerV2 implements Listener {
                 ex = loc.getX();
                 ey = loc.getY();
                 ez = loc.getZ();
-                if ((bx-.75 <= ex && ex <= bx+1.75) && (bz-.75 <= ez && ez <= bz+1.75) && (by-2 <= ey && ey <= by+2.5)) {
+                //if ((bx-.75 <= ex && ex <= bx+1.75) && (bz-.75 <= ez && ez <= bz+1.75) && (by-1 <= ey && ey <= by+2.5)) {
+                if ((bx - 0.5 <= ex && ex <= bx + 0.5) && (bz - 0.5 <= ez && ez <= bz + 0.5) && (by - 1 <= ey && ey <= by + 1)) {
                     // entity is close enough, set target and stop
-                    target = e;
                     e.damage(200);
-                    player.sendMessage("doe");
                     return;
                 }
             }
